42fecfdc8507ab17310e2794d4b68b59c9c67f26
Shakil Ahmed <shakil200409@gmail.com>
2024-09-28 12:22:44 +0600

    Added home page

--- JS/login.js
+++ JS/login.js
@@ -1,11 +1,17 @@
 document.getElementById('btn-login').addEventListener('click', function(event){
     // Prevent the reloading of browser by default
     event.preventDefault();
-    console.log('Login button clicked');
 
     // Get the value of input field
     const phoneNumber = document.getElementById('phone-number').value;
-    console.log(phoneNumber);
+    const pinNumber = document.getElementById('pin-number').value;
+    
+    // Check is it phone number and pin
+    if(phoneNumber === '5' && pinNumber === '1234'){
+        window.location.href = '/home.html';
 
-
+    }
+    else{
+        alert("Wrong Number or Pin");
+    }
 })
