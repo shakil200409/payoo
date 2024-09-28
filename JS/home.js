45ba903cf830bdc175e667bec3897774282241aa
Shakil Ahmed <shakil200409@gmail.com>
2024-09-28 22:29:20 +0600

    Added the full functionality on add money

--- JS/home.js
+++ JS/home.js
@@ -1,9 +1,22 @@
 document.getElementById('btn-add-money').addEventListener('click', function(event){
     event.preventDefault();
     
-    // Get the amount and pin
+    // Get the balance and (amount and pin)
+    const balance = document.getElementById('balance').innerText;
     const addAmount = document.getElementById('amount-input').value;
     const pin = document.getElementById('pin-input').value;
 
-    console.log(addAmount, pin);
+    // Convert string to number
+    const balanceNumber = parseFloat(balance);
+    const addAmountNumber = parseFloat(addAmount);
+
+    if(pin === '1234'){
+        const newBalance = balanceNumber + addAmountNumber;
+
+        // Putting new Balance inside the current balance
+        document.getElementById('balance').innerText = newBalance;
+    }
+    else{
+        alert('Wrong input! Please try again.')
+    }
 })
